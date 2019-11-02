a087742ddb667947cb3ca5318df83d410ac9c1d6
Fran <fmjerez@icloud.com>
2019-11-02 16:02:43 +0100

    Estructure & Background

---
+++ js/background.js
@@ -0,0 +1,26 @@
+class Background {
+    constructor(ctx, width, height){
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
+
+        this.image = new Image();
+        this.image.src = '../img/ladrillos.png';//fondo de ladrillos
+
+        this.posX = 0;
+        this.posY = 0;
+
+        this.vx = 8; //velocidad del eje X a la que se desplazará el fondo
+    }
+
+    draw(){
+        this.ctx.drawImage(this.image, this.posX, this.posY, this.width, this.height);
+        this.ctx.drawImage(this.image, (this.posX + this.width), this.posY, this.width, this.height)//Esta segunda imágen es para que aparezca a continuación de la otra y haga el efecto de movimiento continuo.
+    }
+
+    move() {
+        this.posX -= this.vx
+
+        if(this.posX <= -this.width) this.posX = 0;
+    }
+}
